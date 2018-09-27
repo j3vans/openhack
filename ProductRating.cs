4541c128818dc81de73b239b6390eaadca854b53
Mohamed Zedan <Mohamed.Zedan@novachem.com>
2018-09-27 12:41:42 -0600

    string ID

--- ProductRating.cs
+++ ProductRating.cs
@@ -4,13 +4,12 @@ namespace OpenHackTeam16
 {
     public class ProductRating
     {
-        public string id { get; set; }
         public string UserId { get; set; }
         public string ProductId { get; set; }
         public string LocationName { get; set; }
         public int Rating { get; set; }
         public string UserNotes { get; set; }
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public DateTime TimeStamp { get; set; }
     }
 }
