b66975c50e71e1a35461df83de01cbb66c78ee08
Justin Evans <justin.evans@utah.edu>
2018-09-27 09:58:59 -0400

    azure function app 1

---
+++ JustinFunctionApp1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace BFYOC.GetProduct
+{
+    public static class JustinFunctionApp1
+    {
+        [FunctionName("JustinFunctionApp1")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string productId = req.Query["productId"];
+
+            // string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            // dynamic data = JsonConvert.DeserializeObject(requestBody);
+            // productId = productId ?? data?.productId;
+
+            log.LogInformation($"User passed");
+
+            if (!string.IsNullOrEmpty(productId))
+            {
+                log.LogInformation($"User POST data: {productId}");
+                return (ActionResult)new OkObjectResult($"The product productId for your product id {productId} is Starfruit Explosion and the description is This starfruit ice cream is out of this world!");
+            }
+            else
+            {
+                log.LogError("User did not pass a valid productId");
+                return new BadRequestObjectResult("Please pass a productId on the query string");
+            }
+        }
+    }
+}
