7bc8debb4845fb9b070b2b6dbd0f34cbcd7870fe
Guillermo Londono <glondono@georgiasbdc.org>
2018-09-27 13:18:46 -0400

    base classes for DB

---
+++ ProductRating.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace OpenHackTeam16
+{
+    public class ProductRating
+    {
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+        public string LocationName { get; set; }
+        public int Rating { get; set; }
+        public string UserNotes { get; set; }
+        public Guid Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ ProductRatings.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace OpenHackTeam16
+{
+    public class ProductRatings
+    {
+        public void Init()
+        {
+
+        }
+
+        public ProductRating Get(Guid id)
+        {
+            return null;
+        }
+
+        public void Add(ProductRating rating)
+        {
+
+        }
+
+        public IEnumerable<ProductRating> All()
+        {
+            return null;
+        }
+
+        public void Delete(Guid id)
+        {
+
+        }
+    }
+}
+
